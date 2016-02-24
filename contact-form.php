f09b25e4fbdaf080714f5a1036edc549ed16ed62
StefanSmitshoek <stefansmitshoek@gmail.com>
2016-02-24 15:52:30 +0100

    Stefan
    Hoi

--- contact-form.php
+++ contact-form.php
@@ -27,21 +27,43 @@ if(mail==true){
 else{
   echo "er is niks gestuurt";
 }*/
+$selectOption = $_POST['contact_id'];
+if ($selectOption == 1) {
+  $ontvanger = "Sales department";
+}
+elseif ($selectOption == 2 ) {
+  $ontvanger = "Technical Support and Regulatory affairs";
+}
+elseif ($selectOption == 3 ) {
+  $ontvanger = "Customer Service Department";
+}
+elseif ($selectOption == 4 ) {
+  $ontvanger = "Purchasing Department";
+}
+elseif ($selectOption == 5 ) {
+  $ontvanger = "Other Questions";
+}
 
-if(isset($_POST['submit'])){
+
+
+  if (!isset($_POST["name"])||!isset($_POST["subject"])||!isset($_POST["email"])||!isset($_POST["text"])||!isset($_POST["contact_id"])) {
+      echo "1";
+    }else{
     $to = "peperstuf@gmail.com"; // this is your Email address
     $email = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['first_name'];
-    $subject = "Form submission";
-    $text = $name . " " . " wrote the following:" . "\n\n" . $_POST['text'];
+    $name = $_POST['name'];
+    $subject = $ontvanger;
+    $text =  $name." "."Wrote:". "\n\n".$_POST['text'];
     $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['text'];
 
-    $headers = "From:" . $from;
+    $headers = "From:" . $email;
     $headers2 = "From:" . $to;
     mail($to,$subject,$text,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+    //header('Location: home/newpenguins/public_html/delamine/contact.html');
+    echo "2";
+  }
+
 
  ?>
